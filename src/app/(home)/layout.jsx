0c0e5b8895eb5076278a421f05bb5febfb917499
arthurdev <arthurmartins.dev2025@gmail.com>
2025-12-21 18:39:31 -0300

    feat: add metadata in layout homepage

--- src/app/(home)/layout.jsx
+++ src/app/(home)/layout.jsx
@@ -1,5 +1,10 @@
 import "../styles/globals.css";
 
+export const metadata = {
+  title: "Web of Dungeons",
+  description: "Um RPG dungeon dark baseado na web",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
