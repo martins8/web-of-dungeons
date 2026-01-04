ed6432044a773c89fabb1d081db6ab674da54c93
arthurdev <arthurmartins.dev2025@gmail.com>
2026-01-04 19:42:25 -0300

    feat: add health game component and her test

---
+++ src/game/gcomponents/health.js
@@ -0,0 +1,29 @@
+export default class Health {
+  constructor(amount) {
+    this._currentHp = amount;
+    this._maxHp = amount;
+  }
+
+  get currentHp() {
+    return this._currentHp;
+  }
+  get maxHp() {
+    return this._maxHp;
+  }
+
+  takeDamage(amount) {
+    this._currentHp = Math.max(0, this._currentHp - amount);
+  }
+
+  heal(amount) {
+    this._currentHp = Math.min(this._maxHp, this._currentHp + amount);
+  }
+
+  increaseMaxHealth(amount) {
+    this._maxHp += amount;
+  }
+
+  isAlive() {
+    return this._currentHp > 0;
+  }
+}
---
+++ src/tests/units/game/gcomponents/health.test.js
@@ -0,0 +1,27 @@
+import Health from "src/game/gcomponents/health";
+
+describe("HEALTH TESTS", () => {
+  test("hp never goes below zero", () => {
+    const health = new Health(50);
+    health.takeDamage(999);
+    expect(health.currentHp).toBe(0);
+  });
+  test("hp never exceeds max hp", () => {
+    const health = new Health(50);
+    health.takeDamage(20);
+    health.heal(30);
+    expect(health.currentHp).toBe(50);
+  });
+
+  test("increase hp need to change max hp", () => {
+    const health = new Health(50);
+    health.increaseMaxHealth(5);
+    expect(health.maxHp).toBe(55);
+  });
+
+  test("player needs to die if currentHp < 0", () => {
+    const health = new Health(50);
+    health.takeDamage(55);
+    expect(health.isAlive()).toBe(false);
+  });
+});
