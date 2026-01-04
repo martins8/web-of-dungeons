e884e7808da4e9c1b92e206c5aa13f2a2b8c484b
arthurdev <arthurmartins.dev2025@gmail.com>
2026-01-04 18:19:23 -0300

    feat: create player entity

---
+++ src/game/entities/player.js
@@ -0,0 +1,23 @@
+class Player {
+  constructor(name) {
+    this.validateName(name);
+    this.name = name;
+  }
+
+  validateName(name) {
+    if (typeof name !== "string") {
+      throw new Error("Name must be a string");
+    }
+    if (name.trim().length === 0) {
+      throw new Error("Name cannot be empty");
+    }
+    if (/\d/.test(name)) {
+      throw new Error("Name cannot contain numbers");
+    }
+    if (/\s/.test(name)) {
+      throw new Error("Name cannot contain spaces");
+    }
+  }
+}
+
+export default Player;
